40346eb9961f32242242a371a20e8d4542cabc62
michaelwoo <michaelwoo99@yahoo.com>
2017-02-03 12:33:33 -0500

    import csv

--- db_builder.py
+++ db_builder.py
@@ -1,5 +1,10 @@
 from pymongo import MongoClient
+import csv
+
+coursesfile = open('courses.csv','r')
+peepsfile = open('peeps.csv','r')
 
 server = MongoClient('149.89.150.100')
 db = server.modusoperandi
 col = db['students']
+
