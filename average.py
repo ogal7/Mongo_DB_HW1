92c41905dfedf102fd6d3f6e2c95eb15e56ad2a0
mwoo314 <michaelwoo99@yahoo.com>
2017-02-08 00:28:13 -0500

    average

--- average.py
+++ average.py
@@ -6,11 +6,21 @@ db = server.modusoperandi
 collection = db['students']
 classes = ['systems', 'softdev', 'ceramics', 'greatbooks']
 
+#print collection.count()
+#cursor1 = collectionfind()
+#for i in cursor1:
+#	print i
+#	if "name" in i:
+#		print i["name"]
+
 cursor = collection.find()
 for student in cursor:
-    avg = 0
+    gradeSum = 0.0
+    counter = 0.0
     for course in classes:
-        avg += student[course]
-    dict = {'average': avg}
+    	if course in student:
+	        avg += student[course]
+	        counter += 1
+    avg = gradeSum/counter
     student['average'] = avg
-    print student['average']
+    print "Name:"+student["name"] +"\nID:"+student["id"]+ "\nAverage:"+student["average"] +"\n"
